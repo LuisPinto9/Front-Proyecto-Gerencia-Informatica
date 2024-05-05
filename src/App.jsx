c8876e88da91182e331b54e821743ca8067a074a
binmex <robin019@hotmail.com>
2024-05-05 17:37:32 -0500

    save token into localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "./components/login";
+import Login from "./Components/login";
 
 function App() {
   return (
--- src/views/dashboard.jsx
+++ src/views/dashboard.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { SaveLocalStorage } from '../helpers/SaveLocalStorage';
 
 const dashboard = () => {
+  const location = useLocation();
+  useEffect(()=>{
+    const searchParams = new URLSearchParams(location.search)
+    const token = searchParams.get('token')
+    console.log(token);
+    SaveLocalStorage("token",token)
+  },[location])
   return (
     <div>dashboard</div>
   )
