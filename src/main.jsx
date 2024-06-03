1796f565766e937af4f71ae72b8e5e61b33088d8
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-06-03 10:51:26 -0500

    Refactor file and component names to follow naming conventions

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./routes/router";
-import "./index.css";
+import "./assets/css/index.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 
