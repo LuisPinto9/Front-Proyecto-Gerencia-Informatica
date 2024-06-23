13064a07464b0f260c19a5e624656fcc7752f397
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2024-06-22 19:38:24 -0500

    funcion likes

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -4,12 +4,37 @@ import { useState, useEffect } from "react";
 import {Link} from "react-router-dom"
 // fetch(`http://localhost:4000/api/users/${post.userId}`)
 export default function Post({ post }) {
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
   const [imagens] = useState("/images/person/");
   
 
+  const likeHandler = async () => {
+    console.log("entro");
+    try {
+      const response = await fetch(`http://localhost:4000/api/posts/${post._id}/like`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: user._id }), // Envía el userId al servidor
+      });
+      console.log("entro",response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      if (isLiked) {
+        setLike(like - 1);
+      } else {
+        setLike(like + 1);
+      }
+      setIsLiked(!isLiked);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   useEffect(() => {
   fetch(`http://localhost:4000/api/users?userId=${post.userId}`)
@@ -21,8 +46,8 @@ export default function Post({ post }) {
         })
         .then(
           (result) => {
-            console.log(post.userId)
-            console.log(result)
+            // console.log(post.userId)
+            // console.log(result)
             setUser(result);
             
           }
@@ -33,10 +58,10 @@ export default function Post({ post }) {
     }, []);
 
 
-  const likeHandler = () => {
-    setLike(isLiked ? like - 1 : like + 1);
-    setIsLiked(!isLiked);
-  };
+  // const likeHandler = () => {
+  //   setLike(isLiked ? like - 1 : like + 1);
+  //   setIsLiked(!isLiked);
+  // };
   return (
     <div className="post">
       <div className="postWrapper">
@@ -48,12 +73,10 @@ export default function Post({ post }) {
                 src={user.profilePicture || imagens+"9.jpeg"}
                 alt=""
               />
-            </Link>
-            
+            </Link> 
+             
 
-            <span className="postUsername">
-              {user.username }
-            </span>
+            <span className="postUsername"> {user.username } </span>
             <span className="postDate">{post.createdAt}</span>
           </div>
           <div className="postTopRight">
